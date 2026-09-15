dd92a68569853cb1e9bcace13a545757a253cf41
gevget <gevget@gmail.com>
2026-09-15 15:02:20 +0300

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 const nextConfig = {
+  output: "export",
+  basePath,
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
